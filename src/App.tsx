910d5a508697454e224045c436249bfeec220804
andrepat0 <patini929@gmail.com>
2025-02-15 12:33:44 +0100

    refactor: Simplify App layout and enhance global CSS styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import TextEnhancementUI from './components/TextEnhancementUI'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <TextEnhancementUI />
-    </div>
+    <TextEnhancementUI />
   )
 }
 
