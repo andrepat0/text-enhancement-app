392a7417ae95a4e4905f43dfe38ddc4654b366c4
andrepat0 <patini929@gmail.com>
2025-02-15 12:44:12 +0100

    chore: Update base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
-  base: '/text-enhancement-ui/',
+  base: '/text-enhancement-app/',
   plugins: [react()],
   resolve: {
     alias: {
