a5a867c3b5741e3c509c4dd90a4ca87996320c83
andrepat0 <patini929@gmail.com>
2025-02-20 18:52:22 +0100

    chore: Update Vite base configuration to root path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
-  base: '/text-enhancement-app/',
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
